227f33f25939be189bf9dff48eceb7681228e0fc
Merlin (they/them) <merlinpatt@gmail.com>
2021-04-29 08:46:56 -0400

    Add / route

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import SlackBot from 'slackbots';
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 const router = express.Router();
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+router.get('/', (request, response) => {
+  response.sendFile('index.html');
+});
 
 router.post('/events', (request, response) => {
   const { type } = request.body;
@@ -19,9 +25,13 @@ router.post('/events', (request, response) => {
   }
 
   if (type === 'url_verification') {
-    console.log('URL verification', request.body.challenge);
+    console.info('URL verification', request.body.challenge); // eslint-disable-line no-console
     return response.send(request.body.challenge);
   }
+});
+
+app.listen(PORT, () => {
+  console.info(`Server started on port ${PORT}`); // eslint-disable-line no-console
 });
 
 app.use('/', router);
