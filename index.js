c873ea572bff463245c09c0597527bad1eeab483
Merlin (they/them) <merlinpatt@gmail.com>
2021-04-29 08:38:55 -0400

    Add route

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import express from 'express';
 import SlackBot from 'slackbots';
 
 dotenv.config();
+
+const router = express.Router();
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+router.post('/events', (request, response) => {
+  const { type } = request.body;
+
+  if (!type) {
+    return response.send(500);
+  }
+
+  if (type === 'url_verification') {
+    console.log('URL verification', request.body.challenge);
+    return response.send(request.body.challenge);
+  }
+});
