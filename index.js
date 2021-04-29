f7ca3d5bcada9dc95d5835518800a74b44f4191e
Merlin (they/them) <merlinpatt@gmail.com>
2021-04-29 08:51:06 -0400

    Create static public folder

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const router = express.Router();
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
